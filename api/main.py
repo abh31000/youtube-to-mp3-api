df163ba065d4a534d34414c76279471579ec057d
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-08-16 20:30:43 +0100

    Added playlist support

--- api/main.py
+++ api/main.py
@@ -25,6 +25,14 @@ async def get_song(url:str):
         song.stream_to_buffer(buffer)
 
         return Response(content=buffer.getvalue(), media_type="audio/mpeg")
-
     except:
         return "Invalid url"
+
+
+@app.get("/playlist-urls")
+async def get_songs(url:str):
+    try:
+        p = pt.Playlist(url)
+        return p.video_urls
+    except:
+        return "Invalid playlist url"
