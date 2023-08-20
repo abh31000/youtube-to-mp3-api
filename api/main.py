927e82889cb8b80d49b193e50068169577b4cae9
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-08-20 17:13:02 +0100

    Improved error handling

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 import io
 import pytube as pt
+from pytube.exceptions import *
 
 app = FastAPI()
 
@@ -23,10 +24,28 @@ async def get_song(url:str):
         song = yt.streams.filter(only_audio=True).first()
         buffer = io.BytesIO()
         song.stream_to_buffer(buffer)
-
         return Response(content=buffer.getvalue(), media_type="audio/mpeg")
-    except:
-        return "Invalid url"
+    except AgeRestrictedError:
+        raise HTTPException(status_code=400, detail="Video is age restricted, and cannot be accessed without OAuth.")
+    except ExtractError:
+        raise HTTPException(status_code=400, detail="Couldn't extract data from URL")
+    except HTMLParseError:
+        raise HTTPException(status_code=400, detail="HTML could not be parsed")
+    except LiveStreamError:
+        raise HTTPException(status_code=400, detail="Video is a live stream")
+    except MaxRetriesExceeded:
+        raise HTTPException(status_code=400, detail="Maximum number of retries exceeded")
+    except MembersOnly:
+        raise HTTPException(status_code=400, detail="Video is members-only")
+    except RegexMatchError:
+        raise HTTPException(status_code=400, detail="Regex pattern did not return any matches")
+    except VideoPrivate:
+        raise HTTPException(status_code=400, detail="Video is private")
+    except VideoRegionBlocked:
+        raise HTTPException(status_code=400, detail="Video is region blocked")
+    except VideoUnavailable:
+        raise HTTPException(status_code=400, detail="Video is unavailable")
+        
 
 
 @app.get("/playlist-urls")
@@ -35,4 +54,4 @@ async def get_songs(url:str):
         p = pt.Playlist(url)
         return p.video_urls
     except:
-        return "Invalid playlist url"
+        raise HTTPException(status_code=400, detail="Invalid playlist url")
